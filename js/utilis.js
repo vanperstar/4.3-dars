d124be34714aab550c0b0ebb1d4e23f5869fb41a
v@nperstar <102234460+vanperstar@users.noreply.github.com>
2022-06-05 18:41:41 +0500

    movies genres js

--- js/utilis.js
+++ js/utilis.js
@@ -1,11 +1,17 @@
-function normalizeGenres(movies){
-    let genres = []
-    movies.forEach(movie =>{
-        movie.genres.forEach((genre) => {
-            if(!genres.includes(genre))genres.push(genre)
-        })
-    })
+// function normalizeGenres(movies){
+//     let genres = []
+//     movies.forEach(movie =>{
+//         movie.genres.forEach((genre) => {
+//             if(!genres.includes(genre))genres.push(genre)
+//         })
+//     })
 
-    return genres 
-}
-console.log(normalizeGenres(movies))
+//     return genres 
+// }
+// console.log(normalizeGenres(movies))
+
+function normalizeGenres(movies){
+    let genres = movies.map(movie => movie.genres)
+    return(genres)
+    }
+    normalizeGenres(movies)
